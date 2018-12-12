355246de491015cf320724b13fb8b06e44de694e
saturnstory <45096731+saturnstory@users.noreply.github.com>
2018-12-12 18:20:06 +0300

    Test

--- app/Http/Controllers/StokController.php
+++ app/Http/Controllers/StokController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 class StokController extends Controller
 {
     public function lists(){
+        
+        Uretici::limit(100)->get();
 
         return Stok::where("stokno",">",0)->limit(20)->get();
 
