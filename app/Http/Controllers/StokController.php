c40405e232c5cc8ad0c2a6dd5ffdcf9e5f4633b8
saturnstory <45096731+saturnstory@users.noreply.github.com>
2018-12-13 17:30:41 +0300

    Update StokController.php

--- app/Http/Controllers/StokController.php
+++ app/Http/Controllers/StokController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class StokController extends Controller
 {
     public function lists(){
-        
+        //Deneme
         Uretici::limit(100)->get();
 
         return Stok::where("stokno",">",0)->limit(20)->get();
