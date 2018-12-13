46d5b3d0b4756127df9700b47e023348801a829b
saturnstory <45096731+saturnstory@users.noreply.github.com>
2018-12-13 18:06:05 +0300

    Deneme
    123

--- app/Http/Controllers/StokController.php
+++ app/Http/Controllers/StokController.php
@@ -9,17 +9,6 @@ class StokController extends Controller
 {
     public function lists(){
         //Deneme
-        Uretici::limit(100)->get();
-        
-        //Deneme içerik eklendi
-
-        
-        $name = "test git test";
-      
-
-        $deneme="test";
-
-
         return Stok::where("stokno",">",0)->limit(20)->get();
 
     }
